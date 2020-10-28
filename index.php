4dd52e89638dc11e914747615711a6a52c3d30c0
gibmyx <gibmyxjobs@gmail.com>
2020-10-27 23:21:43 -0400

    example 3

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
-//require_once 'header.php';
-//require_once 'Examples/Example1.php';
-//require_once 'Examples/Example2.php';
+require_once 'header.php';
+require_once 'Examples/Example1.php';
+require_once 'Examples/Example2.php';
 require_once 'Examples/Example3.php';
 
 
